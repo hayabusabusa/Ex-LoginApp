b6fdae42c3f0dcf5c9f4a3fd05ef74f533fa8250
Yamada Shunya <s-yamada@finewerk.co.jp>
2019-04-22 14:58:22 +0900

    Fix main.storyboard

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var identifierTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func tapLoginButton(_ sender: Any) {
-    }
-    
 }
 
